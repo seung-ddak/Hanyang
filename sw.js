c72b8bc2024b186eb3195bcddb1282e5bdfb0251
seunghoon <seunghoon1113@naver.com>
2026-09-16 01:13:11 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='hanyang-study-pwa-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./study_schedule.json','./lessons.html','./full_guide.pdf','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(cs=>cs[0]?cs[0].focus():clients.openWindow('./index.html')))});
